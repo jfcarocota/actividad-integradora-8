81fee7aaa5b7fa7d7280b98c13ec930069181b33
jfcarocota <jfcarocota@gmail.com>
2019-09-09 20:34:36 -0700

    bubble sort added

---
+++ ejercicio01/src/SortingMethods.hh
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include<vector>
+
+class SortingMethods
+{
+private:
+    /* data */
+public:
+    SortingMethods(/* args */);
+    ~SortingMethods();
+    
+    static const std::vector<int> BubbleSort(std::vector<int>);
+};
+
+SortingMethods::SortingMethods(/* args */)
+{
+}
+
+SortingMethods::~SortingMethods()
+{
+}
+
+const std::vector<int> SortingMethods::BubbleSort(std::vector<int> v)
+{
+    int aux = 0;
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < v.size(); j++)
+        {
+            if(v[j] > v[j + 1])
+            {
+                aux = v[j];
+                v[j] = v[j + 1] ;
+                v[j + 1] = aux;
+            }
+        }
+    }
+    
+    return v;
+}
--- ejercicio01/src/main.cc
+++ ejercicio01/src/main.cc
@@ -1,7 +1,16 @@
 #include <iostream>
+#include "SortingMethods.hh"
 
 int main()
 {
-    std::cout << "hello world" << std::endl;
+    std::vector<int> v = {5, 2, 4, 1, 9};
+    std::vector<int> vbubblesorted = SortingMethods::BubbleSort(v);
+    std::cout << "Bubble Sort: " << std::endl;
+    for (int i = 0; i < vbubblesorted.size(); i++)
+    {
+        std::cout << vbubblesorted[i] << std::endl;
+    }
+    
+    
     std::getchar();
 }
