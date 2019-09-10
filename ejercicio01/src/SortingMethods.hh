de082fba4da06f684ef8631974db6bc3e39c747c
jfcarocota <jfcarocota@gmail.com>
2019-09-09 22:22:26 -0700

    bubble sort mejorado y cocktail agregado
    se implemento la funcion swap de stl y se implemento el metodo de cocktail o burbuja bidirecional

--- ejercicio01/src/SortingMethods.hh
+++ ejercicio01/src/SortingMethods.hh
@@ -1,6 +1,7 @@
 #pragma once
-#include <iostream>
+#include<iostream>
 #include<vector>
+#include<utility>
 
 class SortingMethods
 {
@@ -11,6 +12,7 @@ public:
     ~SortingMethods();
     
     static const std::vector<int> BubbleSort(std::vector<int>);
+    static const std::vector<int> CocktailSort(std::vector<int>);
 };
 
 SortingMethods::SortingMethods(/* args */)
@@ -23,19 +25,53 @@ SortingMethods::~SortingMethods()
 
 const std::vector<int> SortingMethods::BubbleSort(std::vector<int> v)
 {
-    int aux = 0;
     for (int i = 0; i < v.size() - 1; i++)
     {
         for (int j = 0; j < v.size() - i - 1; j++)
         {
             if(v[j] > v[j + 1])
             {
-                aux = v[j];
-                v[j] = v[j + 1] ;
-                v[j + 1] = aux;
+                std::swap(v[j], v[j + 1]);
             }
         }
     }
     
     return v;
 }
+
+const std::vector<int> SortingMethods::CocktailSort(std::vector<int> v)
+{
+    int n = v.size();
+    bool swapped = true; 
+    int start = 0; 
+    int end = n - 1; 
+  
+    while (swapped) { 
+        swapped = false; 
+  
+        for (int i = start; i < end; ++i) { 
+            if (v[i] > v[i + 1]) { 
+                std::swap(v[i], v[i + 1]); 
+                swapped = true; 
+            } 
+        } 
+  
+        if (!swapped) 
+            break; 
+  
+        swapped = false; 
+  
+        --end; 
+  
+        for (int i = end - 1; i >= start; --i) { 
+            if (v[i] > v[i + 1]) { 
+                std::swap(v[i], v[i + 1]); 
+                swapped = true; 
+            } 
+        } 
+
+        ++start; 
+    } 
+    return v;
+}
+
--- ejercicio01/src/main.cc
+++ ejercicio01/src/main.cc
@@ -5,11 +5,20 @@ int main()
 {
     std::vector<int> v = {7, 8, 5, 2, 4, 1, 9, 3, 6};
     std::vector<int> vbubblesorted = SortingMethods::BubbleSort(v);
+    std::vector<int> vcocktrailsorted = SortingMethods::CocktailSort(v);
+
     std::cout << "Bubble Sort: " << std::endl;
     for (int i = 0; i < vbubblesorted.size(); i++)
     {
         std::cout << vbubblesorted[i] << std::endl;
     }
+
+    std::cout << "Cocktail Sort: " << std::endl;
+    for (int i = 0; i < vcocktrailsorted.size(); i++)
+    {
+        std::cout << vcocktrailsorted[i] << std::endl;
+    }
+    
     
     
     std::getchar();
